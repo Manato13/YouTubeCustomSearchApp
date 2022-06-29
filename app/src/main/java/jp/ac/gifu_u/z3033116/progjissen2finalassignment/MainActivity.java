98c56e82a3e51f967d925daa37666eb643c839b0
Manato Nakamura <z3033116@edu.gifu-u.ac.jp>
2022-06-29 11:57:11 +0900

    YoutubePlayerAPIの導入。 レイアウトの作成。

--- app/src/main/java/jp/ac/gifu_u/z3033116/progjissen2finalassignment/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/z3033116/progjissen2finalassignment/MainActivity.java
@@ -3,12 +3,41 @@ package jp.ac.gifu_u.z3033116.progjissen2finalassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    private static final String API_KEY = "AIzaSyAP-mnDKDFJLJ8hxPkJzIQN5hvTgctBne8";
+
+    YouTubePlayerView mYouTubePlayerView;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mYouTubePlayerView = findViewById(R.id.Youtube_view);
+
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("BcqxLCWn-CE");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        mYouTubePlayerView.initialize(API_KEY, mOnInitializedListener);
+
     }
 }
